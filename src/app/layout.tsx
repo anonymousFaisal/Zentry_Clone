0c6f251eb93a0f2148bd87eb260501b8e2e940bb
Nahid Hasan <109328601+anonymousFaisal@users.noreply.github.com>
2025-10-06 23:07:09 +0600

    Error solved for the html font section

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,35 +1,35 @@
 import type { Metadata } from "next";
-import './globals.css';
-import localFont from 'next/font/local';
+import "./globals.css";
+import localFont from "next/font/local";
 
 const general = localFont({
-  src: '../fonts/general.woff2',
-  variable: '--font-general',
-  display: 'swap',
+  src: "../fonts/general.woff2",
+  variable: "--font-general",
+  display: "swap",
 });
 
 const circular = localFont({
-  src: '../fonts/circularweb-book.woff2',
-  variable: '--font-circular',
-  display: 'swap',
+  src: "../fonts/circularweb-book.woff2",
+  variable: "--font-circular",
+  display: "swap",
 });
 
 const robertRegular = localFont({
-  src: '../fonts/robert-regular.woff2',
-  variable: '--font-robert-regular',
-  display: 'swap',
+  src: "../fonts/robert-regular.woff2",
+  variable: "--font-robert-regular",
+  display: "swap",
 });
 
 const robertMedium = localFont({
-  src: '../fonts/robert-medium.woff2',
-  variable: '--font-robert-medium',
-  display: 'swap',
+  src: "../fonts/robert-medium.woff2",
+  variable: "--font-robert-medium",
+  display: "swap",
 });
 
 const zentry = localFont({
-  src: '../fonts/zentry-regular.woff2',
-  variable: '--font-zentry',
-  display: 'swap',
+  src: "../fonts/zentry-regular.woff2",
+  variable: "--font-zentry",
+  display: "swap",
 });
 
 export const metadata: Metadata = {
@@ -37,14 +37,11 @@ export const metadata: Metadata = {
   description: "Built with Next.js + Tailwind CSS",
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  const htmlClass = `${general.variable} ${circular.variable} ${robertRegular.variable} ${robertMedium.variable} ${zentry.variable}`;
   return (
-    <html lang="en">
-      <body className={`${general.variable} ${circular.variable} ${robertRegular.variable} ${robertMedium.variable} ${zentry.variable} antialiased`}>
+    <html lang="en" className={htmlClass}>
+      <body className= "antialiased">
         {children}
       </body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Hero from "@/components/Hero";
+import About from "./../components/About";
 
 export default function Page() {
   return (
     <main className="relative min-h-screen w-screen overflow-x-hidden">
-      <Hero/>
+      <Hero />
+      <About />
       <section className="z-0 min-h-screen bg-blue-500"></section>
     </main>
   );
