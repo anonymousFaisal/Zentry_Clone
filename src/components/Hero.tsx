925de938d6bf2529dc886f7294f0c2c87efe87e5
Nahid Hasan <109328601+anonymousFaisal@users.noreply.github.com>
2025-10-06 12:20:38 +0600

    Added the hero video playing section

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -11,7 +11,7 @@ const Hero = () => {
   const totalVideos = 4;
   const nextVideoRef = useRef(null);
 
-  const handleVideoLoaded = () => {
+  const handleVideoLoad = () => {
     setLoadedVideos((prev) => prev + 1);
   };
 
@@ -38,10 +38,27 @@ const Hero = () => {
                 muted
                 id="current-video"
                 className="size-64 origin-center scale-150 object-cover object-center"
-                onLoadedData={handleVideoLoaded}
+                onLoadedData={handleVideoLoad}
               />
             </div>
           </div>
+          <video
+            ref={nextVideoRef}
+            src={getVideoSrc(currentIndex)}
+            loop
+            muted
+            id="next-video"
+            className="absolute-center invisible absolute z-20 size-64 object-cover object-center"
+            onLoadedData={handleVideoLoad}
+          />
+          <video
+            src={getVideoSrc(currentIndex === totalVideos - 1 ? 1 : currentIndex)}
+            autoPlay
+            loop
+            muted
+            className="absolute left-0 top-0 size-full object-cover object-center"
+            onLoadedData={handleVideoLoad}
+          />
         </div>
       </div>
     </div>
