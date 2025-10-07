326d88651cd1eaa728fddc0c1d447f71d47dca03
Nahid Hasan <109328601+anonymousFaisal@users.noreply.github.com>
2025-10-07 16:30:32 +0600

    Created the about section with image animation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,6 @@ export default function Page() {
     <main className="relative min-h-screen w-screen overflow-x-hidden">
       <Hero />
       <About />
-      <section className="z-0 min-h-screen bg-blue-500"></section>
     </main>
   );
 }
